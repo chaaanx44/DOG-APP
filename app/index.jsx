c15a94a3a51b16db07ef3c3e319428dd2a906971
JUSTINECUTIIE <Justinepurisima1@gmail.com>
2024-08-15 11:07:43 +0800

    init

--- app/index.jsx
+++ app/index.jsx
@@ -4,20 +4,11 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Christian Jee Gako to Attendance App</Text>
+    <View className="flex-1 items-center justify-center bg-slate-500 ">
+      <Text className="text-5x1 font-bold font-pblack">Attendance</Text>
       <Link href="/profile">Go to Profile</Link>
       <Link href="/register">Go to Registration</Link>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({ 
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
